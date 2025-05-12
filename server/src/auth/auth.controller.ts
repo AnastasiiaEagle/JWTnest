2693dcbb70c13c18769ca364613d62aadc83396f
Dellari0n <anastasia.ea9le@gmail.com>
2025-05-12 21:12:14 +0300

    2

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterRequest } from './dto/register.dto';
+import { LoginRequest } from './dto/login.dto';
+import type { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +10,25 @@ export class AuthController {
 
     @HttpCode(HttpStatus.CREATED)
     @Post('register')
-    async register (@Body() dto: RegisterRequest){
-      return await this.authService.register(dto);
+    async register (@Res({passthrough: true}) res: Response, @Body() dto: RegisterRequest){
+      return await this.authService.register(res, dto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get('login')
+    async login (@Res({passthrough: true}) res: Response, @Body() dto: LoginRequest){
+      return await this.authService.login(res, dto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get('refresh')
+    async refresh (@Req() req: Request, @Res({passthrough: true}) res: Response){
+      return await this.authService.refresh(req, res);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Get('logout')
+    async logout (@Res({passthrough: true}) res: Response){
+      return await this.authService.logout( res);
     }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,27 +1,32 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterRequest } from './dto/register.dto';
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
 import { ConfigService } from '@nestjs/config';
-import { JwtPayload } from './intefaces/jwt.interface';
+import type { JwtPayload } from './intefaces/jwt.interface';
 import { JwtService } from '@nestjs/jwt';
+import { LoginRequest } from './dto/login.dto';
+import type { Request, Response } from 'express';
+import { isDev } from 'src/utils/is-dev.util';
 
 @Injectable()
 export class AuthService {
-    private readonly JWT_SECRET: string
     private readonly JWT_ACCESS_TOKEN_TTL: string
     private readonly JWT_REFRESH_TOKEN_TTL: string
 
-    private readonly jwtService: JwtService
+    private readonly COOKIE_DOMAIN: string
+
+    // private readonly jwtService: JwtService
     constructor( 
         private readonly prismaService: PrismaService, 
-        private readonly configService: ConfigService){
-            this.JWT_SECRET = configService.getOrThrow<string>('JWT_SECRET')
+        private readonly configService: ConfigService,
+        private readonly jwtService: JwtService,){
             this.JWT_ACCESS_TOKEN_TTL = configService.getOrThrow<string>('JWT_ACCESS_TOKEN_TTL')
             this.JWT_REFRESH_TOKEN_TTL = configService.getOrThrow<string>('JWT_REFRESH_TOKEN_TTL')
-        }
+            this.COOKIE_DOMAIN = configService.getOrThrow<string>('COOKIE_DOMAIN')
+        } 
 
-    async register(dto: RegisterRequest){
+    async register(res: Response, dto: RegisterRequest){
         const { name, email, password} = dto;
 
         const existUser = await this.prismaService.user.findUnique({
@@ -42,7 +47,70 @@ export class AuthService {
             }
         });
 
-        return this.generateToken(user.id);
+        return this.auth(res, user.id)
+    }
+
+    async login(res: Response, dto: LoginRequest){
+        const {email, password} = dto
+
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                email,
+            },
+            select: {
+                id: true,
+                password: true
+            }
+        })
+
+        if(!user){
+            throw new NotFoundException("Користувача не знайдено 1")
+        }
+
+        const isValidPassword = await verify(user.password, password);
+
+        if(!isValidPassword){
+            throw new NotFoundException("Користувача не знайдено 2")
+        }
+
+        return this.auth(res, user.id)
+    }
+
+    async refresh(req: Request, res: Response){   // перевірка авторизованого користувача на життя токену
+        const refreshToken = req.cookies['refreshToken']
+        if (!refreshToken || refreshToken === 'refreshToken') {
+            throw new UnauthorizedException('Недійсний refresh-токен')
+        }
+
+        const payload: JwtPayload = await this.jwtService.verifyAsync(refreshToken)
+        if(payload){
+            const user = await this.prismaService.user.findUnique({
+                where: {
+                    id: payload.id
+                },
+                select: {
+                    id: true
+                }
+            })
+
+            if(!user) {
+                throw new NotFoundException('Користувача не знайдено')
+            }
+
+            return this.auth(res, user.id)
+        }
+    }
+
+    async logout(res: Response){  // вихід
+        this.setCookie(res, 'refreshToken', new Date(0))
+        return true
+    }
+
+    private auth(res: Response, id: string){
+        const {accessToken, refreshToken} = this.generateToken(id)
+        this.setCookie(res, refreshToken, new Date(Date.now() + 1000 * 60 * 60 * 24 * 7))
+
+        return {accessToken}
     }
 
     private generateToken(id: string){
@@ -56,5 +124,15 @@ export class AuthService {
             accessToken,
             refreshToken
         }
+    }
+
+    private setCookie(res: Response, value: string, expires: Date) {
+        res.cookie("refreshToken", value, {
+            httpOnly: true,
+            domain: this.COOKIE_DOMAIN,
+            expires,
+            secure: !isDev(this.configService),
+            sameSite: isDev(this.configService) ? 'none' : 'lax'
+        })
     }
 }
